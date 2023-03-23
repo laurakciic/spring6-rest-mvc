def94c19e0094c28322f32af51232e1574e93325
Laura <laurakovacic@outlook.com>
2023-03-23 08:48:20 +0100

    HTTP GET with Spring MVC List Operation

--- src/main/java/com/laurakovacic/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/laurakovacic/spring6restmvc/controller/BeerController.java
@@ -4,15 +4,22 @@ import com.laurakovacic.spring6restmvc.model.Beer;
 import com.laurakovacic.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController     // returns response body in JSON
 public class BeerController {
     private final BeerService beerService;
+
+    @RequestMapping("/api/v1/beer")     // mapping the path to the list of beers
+    public List<Beer> listBeers() {        // view handler is Jackson which will produce JSON response
+        return beerService.listBeers();
+    }
 
     public Beer getBeerById(UUID id) {
 
--- src/main/java/com/laurakovacic/spring6restmvc/services/BeerService.java
+++ src/main/java/com/laurakovacic/spring6restmvc/services/BeerService.java
@@ -2,8 +2,11 @@ package com.laurakovacic.spring6restmvc.services;
 
 import com.laurakovacic.spring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+    List<Beer> listBeers();
+
     Beer getBeerById(UUID id);
 }
