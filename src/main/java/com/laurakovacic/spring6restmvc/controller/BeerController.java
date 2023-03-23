1ba50acbaa1bfaaa4c0564c5f6b8ce50891a3f77
Laura <laurakovacic@outlook.com>
2023-03-23 09:13:33 +0100

    add path parameters - get by id, limiting to work only with GET

--- src/main/java/com/laurakovacic/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/laurakovacic/spring6restmvc/controller/BeerController.java
@@ -4,7 +4,9 @@ import com.laurakovacic.spring6restmvc.model.Beer;
 import com.laurakovacic.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,19 +15,21 @@ import java.util.UUID;
 @Slf4j
 @AllArgsConstructor
 @RestController     // returns response body in JSON
+@RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")     // mapping the path to the list of beers
+    @RequestMapping(method = RequestMethod.GET)     // limiting to work with GET method only
     public List<Beer> listBeers() {        // view handler is Jackson which will produce JSON response
         return beerService.listBeers();
     }
 
-    public Beer getBeerById(UUID id) {
+    @RequestMapping(value = "/{beerId}", method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("beerId") UUID beerId) {
 
         log.debug("Get Beer by Id - in controller");
 
-        return beerService.getBeerById(id);
+        return beerService.getBeerById(beerId);
 
     }
 }
