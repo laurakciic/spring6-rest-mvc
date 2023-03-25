524fd38afcea392b83ee7bce7332a77ff4a9da74
Laura <laurakovacic@outlook.com>
2023-03-25 09:30:19 +0100

    add update beer test

--- src/test/java/com/laurakovacic/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/laurakovacic/spring6restmvc/controller/BeerControllerTest.java
@@ -1,5 +1,6 @@
 package com.laurakovacic.spring6restmvc.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.laurakovacic.spring6restmvc.model.Beer;
 import com.laurakovacic.spring6restmvc.services.BeerService;
@@ -12,11 +13,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.UUID;
+
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(BeerController.class)     // indicating a test splice
@@ -51,6 +54,19 @@ class BeerControllerTest {
                 .contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(beer)))
                 .andExpect(status().isCreated())
                 .andExpect(header().exists("Location"));
+    }
+
+    @Test
+    void updateBeer() throws Exception {
+        Beer beer = beerServiceImpl.listBeers().get(0);
+
+        mockMvc.perform(put("/api/v1/beer/" + beer.getId())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(beer)))
+                .andExpect(status().isNoContent());   // converting beer POJO to a JSON representation
+
+        verify(beerService).updateBeerById(any(UUID.class), any(Beer.class));   // verifies method was called with given parameters
     }
 
     @Test
